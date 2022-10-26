fa892630aec1fa28995aae746e6c4c06ac5347b6
yinqing <1809955731@qq.com>
2022-10-26 22:17:27 +0800

    api文档管理

--- src/main/java/com/yinqing/reggie/config/WebMvcConfig.java
+++ src/main/java/com/yinqing/reggie/config/WebMvcConfig.java
@@ -1,17 +1,28 @@
 package com.yinqing.reggie.config;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import com.yinqing.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
 @Slf4j
 @Configuration
+@EnableKnife4j
+@EnableSwagger2
 public class WebMvcConfig extends WebMvcConfigurationSupport {
     /**
      * 设置静态资源映射
@@ -21,6 +32,8 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         log.info("静态资源开始映射...");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
     }
@@ -39,5 +52,24 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         //将上面的消息转换器对象追加到mvc框架的转换器集合中
         converters.add(0,messageConverter);
 
+    }
+
+    @Bean
+    public Docket createRestApi() {
+        // 文档类型
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.yinqing.reggie.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("瑞吉外卖")
+                .version("1.0")
+                .description("瑞吉外卖接口文档")
+                .build();
     }
 }
--- src/main/java/com/yinqing/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/yinqing/reggie/filter/LoginCheckFilter.java
@@ -37,7 +37,11 @@ public class LoginCheckFilter implements Filter {
                  "/common/**",
                  "/front/**",
                  "/user/sendMsg",
-                 "/user/login"
+                 "/user/login",
+                 "/doc.html",
+                 "/webjars/**",
+                 "/swagger-resources",
+                 "/v2/api-docs"
          };
          //2.判断请求是否需要处理
         boolean check=check(urls,requestURI);
